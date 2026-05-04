0389bcf84a95be987234f7c111b568f66596300d
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2026-05-04 19:34:29 +0000

    task4 unknown to string value as and uppercase conversion

---
+++ src/task4.ts
@@ -0,0 +1,13 @@
+let secretValue: unknown ="typescript is awesome ";
+
+// let upperValue:secretValue
+
+// let upperValue=secretValue as unknown;
+
+let upperValue=secretValue as string;
+
+const result1= upperValue;
+
+// console.log(result1.toUpperCase());
+
+console.log(result1.toUpperCase());
