28f994b1084427ba9e5a7a2dfa20e1e0bab99703
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2026-05-05 14:57:22 +0000

    access modifier example using bankaccount balance

--- src/accessmodifiers.ts
+++ src/accessmodifiers.ts
@@ -1,30 +1,42 @@
 // access >> modify
 
-class BankAccount {
+class BankAccount 
+{
   public readonly userId: number;
   public userName: string;
   private _userBalance: number;
 
-  constructor(userId: number, userName: string, userBalance: number) {
+  constructor(userId: number, userName: string, userBalance: number) 
+  {
     this.userId = userId;
     this.userName = userName;
     this._userBalance = userBalance;
   }
 
-  private addBalance(balance: number) {
+  private addBalance(balance: number) 
+  {
     this._userBalance = this._userBalance + balance;
   }
 }
 
-class StudentBankAccount extends BankAccount {
-  test() {
-    this.userBalance;
+class StudentBankAccount extends BankAccount 
+{
+  test() 
+  {
+    // its private you can't access private variable outside class
+    // this._userBalance; 
   }
 }
 
 const mezbaBhaiAccount = new BankAccount(111, "Mezba", 20);
 
-mezbaBhaiAccount.addBalance(100);
-mezbaBhaiAccount.addBalance(50);
+// can't update addBalance cause property is private
+// mezbaBhaiAccount.addBalance(100);
+// mezbaBhaiAccount.addBalance(50);
 
 console.log(mezbaBhaiAccount);
+
+
+// Output
+
+// BankAccount { userId: 111, userName: 'Mezba', _userBalance: 20 }
