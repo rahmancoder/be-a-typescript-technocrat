095d3b7a757ae53bf23441f62966562756b39695
Md Mustafizur Rahman Sayem <40750318+rahmancoder@users.noreply.github.com>
2026-05-05 15:09:50 +0000

    encapsulation not the perfect examples to understand, need to check more examples

--- src/encapsulation.ts
+++ src/encapsulation.ts
@@ -1,34 +1,41 @@
 // access >> modify
 
-class BankAccount {
+class BankAccount 
+{
   public readonly userId: number;
   public userName: string;
   private _userBalance: number;
 
-  constructor(userId: number, userName: string, userBalance: number) {
+  constructor(userId: number, userName: string, userBalance: number) 
+  {
     this.userId = userId;
     this.userName = userName;
     this._userBalance = userBalance;
   }
 
-  private addBalance(balance: number) {
+  private addBalance(balance: number) 
+  {
     this._userBalance = this._userBalance + balance;
   }
  
-  callHiddenMethod(balance:number){
+  callHiddenMethod(balance:number)
+  {
     this.addBalance(balance)
   }
 
 }
 
-class StudentBankAccount extends BankAccount {
-  test() {
-    this.
+class StudentBankAccount extends BankAccount 
+{
+  test() 
+  {
+    // can't access private
+    // this._userBalance();
   }
 }
 
 const mezbaBhaiAccount = new BankAccount(111, "Mezba", 20);
 
 
-
-mezbaBhaiAccount.
+// no suggestion showing , its private property now
+// mezbaBhaiAccount.
